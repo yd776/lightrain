fa5f7327e079416bdacbe2c7d255f03ea7d41987
yatin2901 <79344352+yatin2901@users.noreply.github.com>
2022-01-09 15:02:41 +0530

    Update lightrain_spider.py

--- lightrain_spider.py
+++ lightrain_spider.py
@@ -4,13 +4,14 @@ import pyshorteners
 
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3029.110 Safari/537.3'}
 
-source = requests.get('https://www.amazon.in/s?k=grocery&dc&crid=1LI9XOOBAFX0&sprefix=grocer%2Caps%2C273&ref=a9_sc_1', headers = headers).text
+source = requests.get('https://www.amazon.in/s?k=laptops&crid=1AMUSK00JZMXU&sprefix=laptop%2Caps%2C239&ref=nb_sb_noss', headers = headers).text
 soup = BeautifulSoup(source, 'lxml')
 
 results = soup.find_all('div', {'data-component-type':'s-search-result'})
 records = []
 
 s = pyshorteners.Shortener().tinyurl
+
 
 def get_info(items):
     atag=items.h2.a
@@ -33,20 +34,11 @@ def get_info(items):
 
     return result
 
+
 for item in results:
     record = get_info(item)
     if record:
         records.append(record)
-
-for i in range(len(records)):
-    print('name :' + records[i][0])
-    print('url :' + records[i][1])
-    print('price :' + records[i][2])
-    print('rating :' + records[i][3])
-    print('review_count :' + records[i][4])
-    print(" ")
- 
-ur_l = 'https://www.amazon.in/Lifelong-LLBC1401-%E2%80%8ETraining-Mudguard-Assembled/dp/B0976WNS2V/ref=sr_1_1_sspa?keywords=cycle&qid=1641710519&sprefix=cyc%2Caps%2C340&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExMUpNWERYM0g5VkVRJmVuY3J5cHRlZElkPUEwOTUyODk5SkFZRVYzNUZWMlBUJmVuY3J5cHRlZEFkSWQ9QTAzMDU4OTQzQ0NJQUYxSlE1SjdZJndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ=='
 
 def get_more_infor(url):
     source_1 = requests.get(url, headers = headers).text
@@ -58,4 +50,13 @@ def get_more_infor(url):
         print(results_0[i].text + ':' + results_1[i].text)
 
 
-#get_more_infor(ur_l)
+for i in range(len(records)):
+    print('name :' + records[i][0])
+    print('url :' + records[i][1])
+    print('price :' + records[i][2])
+    print('rating :' + records[i][3])
+    print('review_count :' + records[i][4])
+    print(' ')
+    get_more_infor(records[i][1])
+    print(" ")
+ 
